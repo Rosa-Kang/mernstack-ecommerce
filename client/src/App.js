246606f49b24bbc179a02af4049aba5a4888271c
Rosa Kang <therosessom@gmail.com>
2023-02-23 08:11:17 -0800

    setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import { useEffect } from 'react';
 // eslint-disable-next-line
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import Home from './scenes/home/Home'
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Home from './scenes/home/Home';
+import Navbar from './scenes/global/Navbar';
+import ItemDetails from './scenes/itemDetails/ItemDetails';
 
 
 const ScrollToTop = () => {
@@ -17,9 +21,13 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home/>}></Route>
+          <Route path="/item/:itemId" element={<ItemDetails/>}></Route>
+          <Route path="checkout" element={<Checkout/>}></Route>
+          <Route path="checkout/success" element={<Confirmation/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
