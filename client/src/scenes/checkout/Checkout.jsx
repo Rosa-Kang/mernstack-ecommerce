76a1873abdf5770eff56836e74f2efbc7eff68c1
Rosa Kang <therosessom@gmail.com>
2023-02-26 12:12:38 -0800

    add ItemDetails and Checkout

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import { Box, Button, Stepper, Step, StepLabel } from '@mui/material';
+import { Formik } from 'formik';
+import { useState } from 'react';
+import * as yup from "yup";
+import { shades } from "../../theme";
 
 const Checkout = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const cart = useSelector((state) => state.cart.cart);
+  const isFirstStep = activeStep === 0;
+  const isSecondStep = activeStep === 1;
+
+  const handleFormSumbit = async (value, actions) => {
+    setActiveStep(activeStep + 1);
+  }
+
+  async function makePayment(value) {
+
+  }
+
   return (
-    <div>Checkout</div>
+    <Box width="80%" m="100px auto">
+      <Stepper active=></Stepper>
+    </Box>
   )
 }
 
